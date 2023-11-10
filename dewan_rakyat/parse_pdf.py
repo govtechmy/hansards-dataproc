b0da361ad5fe66634301f062dae6f7ab3eb2d3f2
Sheng Han Lim <shenghan@gmail.com>
2023-11-11 00:02:58 +0800

    add pipeline_run and major changes to folder structures

--- dewan_rakyat/parse_pdf.py
+++ dewan_rakyat/parse_pdf.py
@@ -19,7 +19,9 @@ import json
 def not_invisible_rect(obj):
     if obj["object_type"] != "rect":
         return True
-    if isinstance(obj["non_stroking_color"], int) or isinstance(obj["non_stroking_color"], float):
+    if isinstance(obj["non_stroking_color"], int) or isinstance(
+        obj["non_stroking_color"], float
+    ):
         return obj["non_stroking_color"] < 0.9
     return not (min(obj["non_stroking_color"]) > 0.9)
 
@@ -30,38 +32,41 @@ def parse_hansard(hansard_date):
     bold = []
     italics = []
     tables = []
-    text = ''
+    text = ""
     base_path = os.path.dirname(os.path.realpath(__file__))
-    dir_path = f"{base_path}/parsed_pdf/{year}/"
+    dir_path = f"{base_path}/parsed_pdf/"
     if not os.path.isdir(dir_path):
         os.mkdir(dir_path)
-    sortable_date = f'{hansard_date[-4:]}-{hansard_date[2:4]}-{hansard_date[:2]}'  # YYYY-MM-DD
+    sortable_date = (
+        f"{hansard_date[-4:]}-{hansard_date[2:4]}-{hansard_date[:2]}"  # YYYY-MM-DD
+    )
     dir_path += f"{sortable_date}/"
     if not os.path.isdir(dir_path):
         os.mkdir(dir_path)
 
     doa_seen = False
     doa_idx = -1
-    with pdfplumber.open(f'{base_path}/src_hansard/{year}/DR-{hansard_date}.pdf') as pdf:
+    with pdfplumber.open(f"{base_path}/src_hansard/new/DR-{hansard_date}.pdf") as pdf:
         for idx, page in enumerate(tqdm(pdf.pages)):
             if not doa_seen:
-                if 'DOA' in page.extract_text():
+                if "DOA" in page.extract_text():
                     doa_seen = True
                     doa_idx = idx
                 else:
                     continue
-            text += page.extract_text() + '\n'  # add newline to separate pages
+            text += page.extract_text() + "\n"  # add newline to separate pages
             if hansard_date == "09072019":
                 # special case where snap tolerance doesn't work
-                current_tables = page.filter(
-                    not_invisible_rect).extract_tables()
+                current_tables = page.filter(not_invisible_rect).extract_tables()
             else:
-                current_tables = page.filter(
-                    not_invisible_rect).extract_tables({"snap_tolerance": 9})
+                current_tables = page.filter(not_invisible_rect).extract_tables(
+                    {"snap_tolerance": 9}
+                )
             if current_tables:
                 # add page number
-                tables += [[idx, idx - doa_idx + 1,
-                            current_tables]]  # first wrapping is for appending. Want to preserve array structure
+                tables += [
+                    [idx, idx - doa_idx + 1, current_tables]
+                ]  # first wrapping is for appending. Want to preserve array structure
                 # the structure is then
                 # [ # master array
                 #   [ page number, page number relative to DOA,
@@ -70,23 +75,27 @@ def parse_hansard(hansard_date):
                 # ]
                 # where the tables is a list (tables) of lists (table) of lists (row) where final items are the cells
 
-            formatted_words = page.extract_words(extra_attrs=['fontname'])
+            formatted_words = page.extract_words(extra_attrs=["fontname"])
             for word in formatted_words:
                 is_bold = 0
-                if 'bold' in word['fontname'].lower():
+                if "bold" in word["fontname"].lower():
                     is_bold = 1
                 is_italic = 0
-                if 'italic' in word['fontname'].lower():
+                if "italic" in word["fontname"].lower():
                     is_italic = 1
-                bold += [is_bold] * len(word['text'])
-                italics += [is_italic] * len(word['text'])
+                bold += [is_bold] * len(word["text"])
+                italics += [is_italic] * len(word["text"])
 
-    assert len(bold) == len(italics), f'Length of bold and italics do not match: {len(bold)} vs {len(italics)}'
-    raw_text = re.sub(r'\s+', '', text)
-    assert len(bold) == len(raw_text), f'Length of bold and raw_text do not match: {len(bold)} vs {len(raw_text)}'
+    assert len(bold) == len(
+        italics
+    ), f"Length of bold and italics do not match: {len(bold)} vs {len(italics)}"
+    raw_text = re.sub(r"\s+", "", text)
+    assert len(bold) == len(
+        raw_text
+    ), f"Length of bold and raw_text do not match: {len(bold)} vs {len(raw_text)}"
     # add whitespace to bold and italics using the raw text layout
-    spaced_bold = ''
-    spaced_italics = ''
+    spaced_bold = ""
+    spaced_italics = ""
     # to improve time complexity, reverse the string and pop from the end
     bold.reverse()
     italics.reverse()
@@ -97,28 +106,31 @@ def parse_hansard(hansard_date):
         else:
             spaced_bold += str(bold.pop())
             spaced_italics += str(italics.pop())
-    assert len(bold) == 0, f'Not all bold characters were processed: {len(bold)}'
-    assert len(italics) == 0, f'Not all italic characters were processed: {len(italics)}'
+    assert len(bold) == 0, f"Not all bold characters were processed: {len(bold)}"
+    assert (
+        len(italics) == 0
+    ), f"Not all italic characters were processed: {len(italics)}"
 
-    with open(dir_path + "plaintext.txt", 'w') as f:
+    with open(dir_path + "plaintext.txt", "w") as f:
         f.write(text)
-    with open(dir_path + "bold.txt", 'w') as f:
+    with open(dir_path + "bold.txt", "w") as f:
         f.write(spaced_bold)
-    with open(dir_path + "italics.txt", 'w') as f:
+    with open(dir_path + "italics.txt", "w") as f:
         f.write(spaced_italics)
-    with open(dir_path + "tables.json", 'w') as f:
+    with open(dir_path + "tables.json", "w") as f:
         json.dump(tables, f, indent=4)
 
     # for global logging
     if tables:
-        with open("hansards_with_tables.txt", 'a') as f:
+        with open("hansards_with_tables.txt", "a") as f:
             f.write(f"{sortable_date}\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("hansard_date", help="hansard_date eg. 23052023",
-                        default="18092023", nargs="?")
+    parser.add_argument(
+        "hansard_date", help="hansard_date eg. 23052023", default="18092023", nargs="?"
+    )
     # Parse arguments
     args = parser.parse_args()
     parse_hansard(args.hansard_date)
---
+++ dewan_rakyat/pipeline_run.py
@@ -0,0 +1,148 @@
+import os
+import parse_pdf
+import pretabulation_processing
+import tabulate_hansard
+
+import shutil
+from tqdm import tqdm
+from pathlib import Path
+
+import edit_hansards
+import post_parsing_edits
+import re
+
+
+def get_filenames_in_folder(folder_path_):
+    filenames_ = list(folder_path_.glob("*.pdf"))
+    return filenames_
+
+
+def rename_pdf_files(paths):
+    for path in paths:
+        match = re.search(r"DR-(\d{2})(\d{2})(\d{4})\.pdf", path.name, re.IGNORECASE)
+        if match:
+            # Extract day, month, year from the match groups
+            day, month, year = match.groups()
+            # Format the new filename
+            new_filename = f"dr_{year}-{month}-{day}.pdf"
+            # Create a new 'upload' directory if it doesn't exist
+            upload_dir = path.parent.parent / "upload"
+            upload_dir.mkdir(exist_ok=True)
+            # Create a new Path object for the renamed file within the 'upload' directory
+            new_path = upload_dir / new_filename
+            # Rename and move the file
+            shutil.copy(path, new_path)
+
+
+def preprocess():
+    # for preprocessing
+    with open("hansards_with_tables.txt", "w") as f:
+        f.write("")
+    with open("errors/hansards_with_parsing_errors.txt", "w") as f:
+        f.write("")
+    for hansard_date in tqdm(hansard_dates):
+        try:
+            parse_pdf.parse_hansard(hansard_date)
+        except Exception as e:
+            print(e)
+            # write this filename to file
+            with open("errors/hansards_with_parsing_errors.txt", "a") as f:
+                f.write(hansard_date + "\n")
+            print("Error parsing " + hansard_date)
+            continue
+
+
+def pre_tabulate():
+    # for pre-tabulation
+    pre_tabulation_files_for_deletion = [
+        "dump/matched_tables.txt",
+        "errors/error_tables.txt",
+        "errors/pretabulation_errors.txt",
+    ]
+    for file in pre_tabulation_files_for_deletion:
+        if os.path.exists(file):
+            os.remove(file)
+    for hansard_date in tqdm(hansard_dates):
+        try:
+            pretabulation_processing.preprocess(hansard_date)
+        except Exception as e:
+            print(e)
+            print(f"Error in {hansard_date}")
+            with open("errors/pretabulation_errors.txt", "a") as f:
+                f.write(f"{hansard_date}\n")
+                f.write(f"{e}\n\n")
+            continue
+
+
+# for tabulation
+def tabulate():
+    # clean these files for new logs
+    tabulation_files_for_deletion = [
+        "dump/autoclosed_annotation.txt",
+        "dump/all_timestamps_dated.txt",
+        "dump/all_timestamps.txt",
+        "warnings/matched_categories.csv",
+        "warnings/timestamp_in_annotation.txt",
+        "warnings/autocorrected_authors.txt",
+        "warnings/stray_bolds.txt",
+        "warnings/capitalised_level_2.txt",
+        "warnings/level_2_following_level_1.txt",
+        "warnings/in-text-bold.txt",
+        "warnings/annotation_too_long.txt",
+        "warnings/uppercased_non_author.txt",
+        "warnings/mixed_bolds.txt",
+        "warnings/unsorted_timestamps.txt",
+        "errors/tabulation_errors.txt",
+    ]
+
+    for file in tabulation_files_for_deletion:
+        if os.path.exists(file):
+            os.remove(file)
+    for hansard_date in tqdm(hansard_dates):
+        try:
+            tabulate_hansard.tabulate(hansard_date)
+        except Exception as e:
+            print(e)
+            print(f"Error in {hansard_date}")
+            with open("errors/tabulation_errors.txt", "a") as f:
+                f.write(f"{hansard_date}\n")
+                f.write(f"{e}\n\n")
+            continue
+
+
+def clean_up():
+    # copy files from 'new' folder to 'done' folder
+    for filename in filenames:
+        shutil.copy(filename, Path.cwd() / "src_hansard" / "done" / filename.name)
+
+    # copy tabulated files to 'tabulated_upload' folder
+    for hansard_date in hansard_dates:
+        new_hansard_date = f"{hansard_date[-4:]}-{hansard_date[2:4]}-{hansard_date[:2]}"
+
+        shutil.copy(
+            Path.cwd() / "tabulated" / f"{new_hansard_date}" / "result.csv",
+            Path.cwd() / "tabulated_upload" / f"dr_{new_hansard_date}.csv",
+        )
+
+
+if __name__ == "__main__":
+    # Pipeline steps:
+    # 1. Download new PDF files into 'new' folder
+    # 2. Rename PDF files
+    # 3. Run all steps
+    # 4. If all good, clean up
+
+    # get list of new PDF files
+    filenames = get_filenames_in_folder(Path.cwd() / "src_hansard" / "new")
+    # rename a copy for uploading to S3
+    rename_pdf_files(filenames)
+
+    hansard_dates = [x.stem[3 : 3 + 8] for x in filenames]
+    preprocess()
+    post_parsing_edits.modify_tables()
+    pre_tabulate()
+    edit_hansards.edit_hansards()
+    tabulate()
+
+    # if all good, clean up by moving filenames to 'done' folder, and tabulated to tabulated_upload folder
+    # clean_up()
