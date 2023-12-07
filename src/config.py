2b3d9424aed9ed46a166a0dbfdb7b66c411a28e3
Sheng Han Lim <shenghan@gmail.com>
2023-12-07 23:34:58 +0800

    fix config paths

--- src/config.py
+++ src/config.py
@@ -8,10 +8,10 @@ HOUSE_NAME = "KKDR"
 BASE_PATH = Path.cwd()
 
 # FOR BATCH RUN: source PDF folder
-DEFAULT_DATA_DIR = Path(
-    "/Users/shenghan/Code/playground/notebooks/hansards/hansard-kkdr-pdf"
-)
-# DEFAULT_DATA_DIR = Path.cwd().parent / "data" / "debug"
+# DEFAULT_DATA_DIR = Path(
+#     "/Users/shenghan/Code/playground/notebooks/hansards/hansard-kkdr-pdf"
+# )
+DEFAULT_DATA_DIR = Path.cwd().parent / "data" / "debug"
 
 # FOR PIPELINE: where new PDF files are expected to be and ingested into pipeline
 INPUT_PIPELINE_DIR = Path.cwd().parent / "data" / "new"
