b84624f3e3207240f118bf9ab74ef6b2ff6a18fc
Asyiqin Nasir <asyiq@MacBook-Pro-2.local>
2026-01-08 14:35:16 +0800

    revert code json dump

--- hansards_pipelines/hansards_pipelines/assets.py
+++ hansards_pipelines/hansards_pipelines/assets.py
@@ -956,7 +956,7 @@ def prepare_db_payload(context: AssetExecutionContext):
         "date": sitting_object["proper_date_str"],
         "filename": sitting_object["renamed_filename"],
         "is_final": is_final,
-        "speech_data": speech_data,
+        "speech_data": json.dumps(speech_data, ensure_ascii=False),
         "house": house_mapper.code_to_display(sitting_object["house"]),
     }
 
